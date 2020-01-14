017b973e521462a3cfb9bc81f22720ae6df82132
Ed <edwoulfe@gmail.com>
2020-01-14 12:22:24 -0600

    Starter files for prep

---
+++ double-equals-vs-triple.js
@@ -0,0 +1,8 @@
+let example1 = 5;
+let example2 = "5";
+
+// Double equals checks the value of the variables.
+console.log(example1 == example2); // true
+
+// Triple equals checks the value and types of the variables.
+console.log(example1 === example2); // false
