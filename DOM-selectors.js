9f476672322c0f35c155c12d7c034f1afe61fcec
Ed <edwoulfe@gmail.com>
2020-01-14 13:09:24 -0600

    Couple more practice files.

---
+++ DOM-selectors.js
@@ -0,0 +1,5 @@
+document.getElementById("heading_one");
+document.getElementsByClassName("para_class");
+document.getElementsByTagName("li");
+document.querySelector(".para_class");
+document.querySelectorAll(".list_item");
