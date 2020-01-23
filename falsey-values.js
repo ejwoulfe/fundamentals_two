f2cb9eb68bc5231e54f0890d7dc252920cd37d06
Ed <edwoulfe@gmail.com>
2020-01-23 15:45:53 -0600

    Going over values which return false in if statements

---
+++ falsey-values.js
@@ -0,0 +1,8 @@
+// Falsey values are values when put into an if statement, will return false.
+
+console.log(Boolean(0)); // false
+console.log(Boolean(false)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(null)); // false
+console.log(Boolean("")); // false
